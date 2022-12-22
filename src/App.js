b80db9071f20ac8a4cfcfd59e9bde7e1d8db2d94
Dmitrii Builin <d.builin@mail.ru>
2022-12-22 13:42:27 +0300

    Added React, made header and footer

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Footer from './pages/footer/footer';
+import Header from './pages/header/header';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
