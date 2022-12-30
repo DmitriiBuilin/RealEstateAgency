6246df6ab6ac40bf8eaa3cf0ae3181636cb1d4ad
Dmitrii Builin <d.builin@mail.ru>
2022-12-30 22:17:32 +0300

    Fixed styles

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -3,37 +3,37 @@ export const Cards = (props) => {
         <>
             <div className="offers-carousel-item">
                 <p className="offers-carousel-item-name">{props.name}</p>
-                <div className="offers-carousel-cart-block">
-                    <div className="offer-cart hid1">
-                        <img className="offer-cart-img" src={props.img_1} alt="..." />
-                        <div className="offer-cart-properties">
-                            <h4 className="offer-cart-name">Name</h4>
-                            <p className="offer-cart-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab quod alias ipsam modi odio repellendus a, explicabo.</p>
-                            <p className="offer-cart-price">Price <span>1200</span> $ </p>
+                <div className="offers-carousel-card-block">
+                    <div className="offer-card hid1">
+                        <img className="offer-card-img" src={props.img_1} alt="..." />
+                        <div className="offer-card-properties">
+                            <h4 className="offer-card-name">Name</h4>
+                            <p className="offer-card-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab quod alias ipsam modi odio repellendus a, explicabo.</p>
+                            <p className="offer-card-price">Price <span>1200</span> $ </p>
                         </div>
                     </div>
-                    <div className="offer-cart hid2">
-                        <img className="offer-cart-img" src={props.img_2} alt="..." />
-                        <div className="offer-cart-properties">
-                            <h4 className="offer-cart-name">Name</h4>
-                            <p className="offer-cart-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab quod alias ipsam modi odio repellendus a, explicabo.</p>
-                            <p className="offer-cart-price">Price <span>1200</span> $ </p>
+                    <div className="offer-card hid2">
+                        <img className="offer-card-img" src={props.img_2} alt="..." />
+                        <div className="offer-card-properties">
+                            <h4 className="offer-card-name">Name</h4>
+                            <p className="offer-card-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab quod alias ipsam modi odio repellendus a, explicabo.</p>
+                            <p className="offer-card-price">Price <span>1200</span> $ </p>
                         </div>
                     </div>
-                    <div className="offer-cart hid3">
-                        <img className="offer-cart-img" src={props.img_3} alt="..." />
-                        <div className="offer-cart-properties">
-                            <h4 className="offer-cart-name">Name</h4>
-                            <p className="offer-cart-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab quod alias ipsam modi odio repellendus a, explicabo.</p>
-                            <p className="offer-cart-price">Price <span>1200</span> $ </p>
+                    <div className="offer-card hid3">
+                        <img className="offer-card-img" src={props.img_3} alt="..." />
+                        <div className="offer-card-properties">
+                            <h4 className="offer-card-name">Name</h4>
+                            <p className="offer-card-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab quod alias ipsam modi odio repellendus a, explicabo.</p>
+                            <p className="offer-card-price">Price <span>1200</span> $ </p>
                         </div>
                     </div>
-                    <div className="offer-cart">
-                        <img className="offer-cart-img" src={props.img_4} alt="..." />
-                        <div className="offer-cart-properties">
-                            <h4 className="offer-cart-name">Name</h4>
-                            <p className="offer-cart-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab quod alias ipsam modi odio repellendus a, explicabo.</p>
-                            <p className="offer-cart-price">Price <span>1200</span> $ </p>
+                    <div className="offer-card">
+                        <img className="offer-card-img" src={props.img_4} alt="..." />
+                        <div className="offer-card-properties">
+                            <h4 className="offer-card-name">Name</h4>
+                            <p className="offer-card-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab quod alias ipsam modi odio repellendus a, explicabo.</p>
+                            <p className="offer-card-price">Price <span>1200</span> $ </p>
                         </div>
                     </div>
                 </div>
